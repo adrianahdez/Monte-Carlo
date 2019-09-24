41db287f53419bfc9a5e6dc461ee4fa25b2576fe
Michel Escalante Álvarez <michel.escalantea@gmail.com>
2019-09-24 17:08:31 +0200

    Updated to solution

--- aproximar_integral_multiple_transp.py
+++ aproximar_integral_multiple_transp.py
@@ -1,41 +1,42 @@
 ##import numpy as np
 ##con numpy
-#import random
+# import random
 
-#nsim=100000
-#integral=0.
-#for i in range(0,nsim):
-    ##x=np.random.rand(8)
-    #x=random.random()
-    #gx=x.sum()**2
-    #integral+=gx
-    
-#integral*=2.**(-7)
-#integral/=float(nsim)
+# nsim=100000
+# integral=0.
+# for i in range(0,nsim):
+##x=np.random.rand(8)
+# x=random.random()
+# gx=x.sum()**2
+# integral+=gx
 
-#error=abs(integral-25./192.)
-#print 'Integral= ', integral, '  error= ', error
-#s='Nsim='+str(nsim)+'     I= '+ str(integral)
+# integral*=2.**(-7)
+# integral/=float(nsim)
 
-#sin numpy
+# error=abs(integral-25./192.)
+# print 'Integral= ', integral, '  error= ', error
+# s='Nsim='+str(nsim)+'     I= '+ str(integral)
+
+# sin numpy
 
 print '\n SIN NUMPY'
 import random
 import math
-nsim=1000000
-integral=0.
-#random.seed(3)
-for i in range(0,nsim):
-    x=[]
-    for k in range(0,100):
-        x.append(random.random()) #U(0,1)
-    gx=sum(x)**2
-    gx2=math.exp(-gx)
-    integral+=gx2
-    
-#integral*=2.**(-7)
-integral/=float(nsim)
 
-#error=abs(integral-25./192.)
-print 'Integral= ', integral#, '  error= ', error
-s='Nsim='+str(nsim)+'     I= '+ str(integral)
+nsim = 1000000
+integral = 0.
+# random.seed(3)
+for i in range(0, nsim):
+    x = []
+    for k in range(0, 100):
+        x.append(random.random())  # U(0,1)
+    gx = sum(x) ** 2
+    gx2 = math.exp(-gx)
+    integral += gx2
+
+# integral*=2.**(-7)
+integral /= float(nsim)
+
+# error=abs(integral-25./192.)
+print 'Integral= ', integral  # , '  error= ', error
+s = 'Nsim=' + str(nsim) + '     I= ' + str(integral)
--- aproximar_pi.py
+++ aproximar_pi.py
@@ -1,47 +1,25 @@
-
-#import numpy as np
-#import matplotlib.pyplot as plt
-#import matplotlib
-#from matplotlib.collections import PatchCollection
-#import matplotlib.patches as mpatches
-
-#fig = plt.figure(figsize=(5,5))
-#ax = plt.subplot(111, aspect=1)
-
-
-#art = mpatches.Rectangle((-1,-1),2,2,facecolor="white",ec='b',alpha=1,lw=1)
-
-#ax.add_patch(art)
-
-#art = mpatches.Circle((0.,0.), 1.,facecolor="y", ec="r", lw=2,alpha=0.25)
-
-#ax.add_patch(art)
-
-
-#ax.set_xticks([])
-#ax.set_yticks([])
-#ax.axis([-1.1,1.1,-1.1,1.1])
-#ax.axis([0,1.05,0,1.05])
-
 import random
-nptos=0
-ptosIn=0
-nsim=100000000
-#random.seed(10)
-for i in range(0,nsim):
-    #x=np.random.rand()
-    #y=np.random.rand()
-   x=random.random()
-   y=random.random()
-   nptos+=1
-   if x**2+y**2<=1:
-     ptosIn+=1
-        #ax.plot([x],[y],'bx',lw=2,ms=5)
-    #else:ax.plot([x],[y],'gx',lw=2,ms=5)
 
-print 'ptosIn= ', ptosIn
-vpi=4.*float(ptosIn)/float(nsim)
-print 'pi= ', vpi
-#s='Nsim='+str(nsim)+'     PI= '+ str(vpi)
-#plt.title(s)
-#plt.show()
+r1Points = 0
+r2Points = 0
+r3Points = 0
+nsim = 10000
+
+random.seed(1)
+
+for i in range(0, nsim):
+    x = random.random()
+    y = random.random()
+
+    if x ** 2 + y ** 2 <= 1:
+        r1Points += 1
+        if -.6 <= x <= .6:
+            r2Points += 1
+            if x ** 2 + y ** 2 >= .5 ** 2:
+                r3Points += 1
+
+r1 = 4. * float(r1Points) / float(nsim)
+r2 = 4. * float(r2Points) / float(nsim)
+r3 = 4. * float(r3Points) / float(nsim)
+
+print('R1 Area = ', r1, '\nR2 Area = ', r2, '\nR3 Area = ', r3)
